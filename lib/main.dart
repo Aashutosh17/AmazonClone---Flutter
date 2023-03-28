ab74f922f0a16a3a929429e212a3488c46d73ccf
Aashutosh17 <78782279+Aashutosh17@users.noreply.github.com>
2023-03-28 15:33:16 +0530

    folder structure, routes - Aashutosh

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:amazonclone/constants/global_variables.dart';
+import 'package:amazonclone/features/auth/auth_screen.dart';
+import 'package:amazonclone/router.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Amazon Clone',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+        ),
+      ),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: const AuthScreen(),
+    );
+  }
+}
