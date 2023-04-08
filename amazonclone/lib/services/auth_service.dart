3c6b10aa03245c29ce691ff989aad7b331380932
Aashutosh17 <78782279+Aashutosh17@users.noreply.github.com>
2023-04-08 23:26:43 +0530

    Error handling, Auth Services, User models for Signup

--- amazonclone/lib/services/auth_service.dart
+++ amazonclone/lib/services/auth_service.dart
@@ -1,15 +1,36 @@
+import 'package:amazonclone/constants/global_variables.dart';
+import 'package:amazonclone/models/user.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 class AuthService {
+  get onSucess => null;
+
   // a function to signing up the user
 
   void signUpUser({
+    required BuildContext context,
     required String email,
     required String password,
     required String name,
   }) async {
-    try{
+    try {
+      User user = User(
+          id: '',
+          name: name,
+          email: email,
+          password: password,
+          address: '',
+          type: '',
+          token: '');
 
-    }catch(e){
-      
-    }
+      http.Response res = await http.post(
+        Uri.parse('$uri/api/signup'),
+        body: user.toJson(),
+        headers: <String, String>{
+          'content-Type': 'application/json; charset = UTF-8',
+        },
+      );
+    } catch (e) {}
   }
 }
