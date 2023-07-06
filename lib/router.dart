bebb4456db573c53efb0c02c73e4d3cb1151678b
Aashutosh17 <78782279+Aashutosh17@users.noreply.github.com>
2023-07-06 11:45:24 +0545

    getting product based on category

--- lib/router.dart
+++ lib/router.dart
@@ -1,6 +1,7 @@
 import 'package:amazonclone/common/widgets/bottom_bar.dart';
 import 'package:amazonclone/features/admin/screens/add_product_screen.dart';
 import 'package:amazonclone/features/auth/screens/auth_screen.dart';
+import 'package:amazonclone/features/home/screens/category_deals_screen.dart';
 import 'package:amazonclone/features/home/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -29,6 +30,15 @@ Route<dynamic> generateRoute(RouteSettings routeSettings) {
       return MaterialPageRoute(
         settings: routeSettings,
         builder: (_) => const AddProductScreen(),
+      );
+
+    case CategoryDealsScreen.routeName:
+      var category = routeSettings.arguments as String;
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => CategoryDealsScreen(
+          category: category,
+        ),
       );
 
     default:
