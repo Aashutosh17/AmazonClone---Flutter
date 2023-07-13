974511e3c0d78c404d3ff3e97ddae68553957223
Aashutosh17 <78782279+Aashutosh17@users.noreply.github.com>
2023-07-13 19:04:59 +0545

    apple pay

--- lib/router.dart
+++ lib/router.dart
@@ -1,4 +1,5 @@
 import 'package:amazonclone/common/widgets/bottom_bar.dart';
+import 'package:amazonclone/features/address/screens/address_screen.dart';
 import 'package:amazonclone/features/admin/screens/add_product_screen.dart';
 import 'package:amazonclone/features/auth/screens/auth_screen.dart';
 import 'package:amazonclone/features/home/screens/category_deals_screen.dart';
@@ -59,6 +60,14 @@ Route<dynamic> generateRoute(RouteSettings routeSettings) {
         settings: routeSettings,
         builder: (_) => ProductDetailScreen(
           product: product,
+        ),
+      );
+    case AddressScreen.routeName:
+      var totalAmount = routeSettings.arguments as String;
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => AddressScreen(
+          totalAmount: totalAmount,
         ),
       );
 
